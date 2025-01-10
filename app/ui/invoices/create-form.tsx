df71dfdce808be687e4897c165297ea9c2993749
lte45 <lesley.earle@gmail.com>
2025-01-10 09:49:08 -0500

    merging create/edit actions for cleanliness

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,11 +7,12 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import {createInvoice} from '@/app/lib/actions';
+import {createOrEditInvoice} from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const createInvoiceWithNullId = createOrEditInvoice.bind(null,'0');
   return (
-    <form action={createInvoice}>
+    <form action={createInvoiceWithNullId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
